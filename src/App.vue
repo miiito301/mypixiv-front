bd1bff6cba7dd111371fdd58968da018ca498daf
miiito301 <ikaruga0131@gmail.com>
2025-08-22 16:42:59 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<!--App.vue-->
+  <div>
+    <nav>
+      <router-link to="/form">作品登録</router-link> <!--router-link は「aタグのようにURLを切り替える専用タグ」-->
+      <router-link to="/search">作品検索</router-link>
+    </nav>
+    <router-view /><!--router-view は「画面切り替えの表示場所」-->
+  </div>
+</template>
+
+<script setup>
+
+</script>
