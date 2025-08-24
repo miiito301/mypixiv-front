c2c4ff899f3df5790f038f255ff9e9adc6e3b2e6
miiito301 <ikaruga0131@gmail.com>
2025-08-24 17:26:42 +0900

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from '../router/index.js';
-
-createApp(App).use(router).mount('#app');
-//use(router) でアプリ全体にルーターを組み込む
